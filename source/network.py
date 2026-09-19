8935ea4895ac6f3b010a79c07aa5656e8085d7d5
mariliamr <mariliarmribeiro@gmail.com>
2026-09-19 00:12:00 -0300

    Add socket functions for network communication
    Criação dos sockets, e definição das funções "mandar mensagem" e "receber mensagem"

--- source/network.py
+++ source/network.py
@@ -1,1 +1,10 @@
+import socket
 
+def criarsocket():
+  return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def mandarmensagem(conexao, mensagem):
+  conexao.send(mensagem.encode("utf-8"))
+
+def recebermensagem(conexao):
+  return conexao.recv(1024).decode("utf-8")
