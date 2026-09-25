ab60bb2d0f42f5780e6c90391f1bd0341508dd69
alvaromc <alvaromcampelo@icloud.com>
2026-09-25 00:02:45 -0300

    Inserted logo on begining of the program. Created /limpar function that clears personal chat history. Other smal aesthetic changes

--- source/client_interface.py
+++ source/client_interface.py
@@ -4,11 +4,22 @@ import network #usado pra criar o socket e mandar/receber mensagens
 import protocol
 from protocol import Mensagem #montar/ler as mensagens em json
 import threading
+import os
 
 HOST = "127.0.0.1" #ip do servidor (troque pelo ip do pc do servidor para conversar pela rede)
 PORTA = 5000 #mesma porta do servidor
 
-def ouvir(conexao): #fica ouvindo(recebendo, procurando) as mensagens do servidor e mostrando na tela
+LOGO = r"""
+ ______    ______      ______              ______      __  __      ______      ______
+/\  == \  /\  ___\    /\__  _\    ______  /\  ___\    /\ \_\ \    /\  __ \    /\__  _\
+\ \  _-/  \ \  __\    \/_/\ \/   /\_____\ \ \ \____   \ \  __ \   \ \  __ \   \/_/\ \/
+ \ \_\     \ \_____\     \ \_\   \/_____/  \ \_____\   \ \_\ \_\   \ \_\ \_\     \ \_\
+  \/_/      \/_____/      \/_/              \/_____/    \/_/\/_/    \/_/\/_/      \/_/
+
+                    chat pelo terminal  |  PET Eng. Comp. UFC
+""" #o r antes faz o python não tratar as barras \ como comando tipo \n
+
+def ouvir(conexao, apelido): #fica ouvindo(recebendo, procurando) as mensagens do servidor e mostrando na tela
     while True:
         try:
             texto = network.recebermensagem(conexao)
@@ -23,11 +34,16 @@ def ouvir(conexao): #fica ouvindo(recebendo, procurando) as mensagens do servido
         except protocol.MensagemInvalida:
             continue
 
-        print(msg)
+        print(f"\n{msg}") #\n pula a linha que o usuario ta escrevendo. screvendo embaixo
+        #aqui foi decisao minha mesmo, ou era isso, ou msg apagava no meio quando alguem enviasse! codigo pra substituir(testar):  print(f"\r{str(msg).ljust(len(apelido) + 2)}")
+        #terceira opcao é nao ter nada, mas achei muito feio, não fica evidente que está sendo esperado um texto
+         
+        print(f"{apelido}: ", end="", flush=True) #mostra o "apelido: " de novo embaixo, esperando novamente a mensagem
 
     print("* conexão encerrada")
 
 def main():
+    print(LOGO)
     apelido = input("Seu apelido: ").strip() or "anonimo" #nome que aparece pros outros
 
     conexao = network.criarsocket() #cria o socket
@@ -38,19 +54,24 @@ def main():
 
     thread = threading.Thread( #executa a função ouvir paralelamente. deixa receber mensagens enquanto o usuario dtambem igita
         target=ouvir,
-        args=(conexao,),
+        args=(conexao,apelido),
         daemon=True #a thread fecha junto com o programa
     )
     thread.start()
 
     while True: #loop pra mandar mensagem
         try:
-            texto = input()
-        except (KeyboardInterrupt, EOFError): #ctrl+c também sai
+            texto = input(f"{apelido}: ") #mostra o apelido antes do que o usuario digita, pra deixar mais facil visualização
+        except (KeyboardInterrupt, EOFError): #ctrl+c tambem sai
             break
 
         if texto == "/sair":
             break
+
+        if texto == "/limpar": #limpa só a tela de quem digitou, 100% estetico
+            os.system("cls" if os.name == "nt" else "clear") #cls no windows, clear em linux
+            print(LOGO) #pra ficar bunitin dnv
+            continue
 
         if texto == "":
             continue
